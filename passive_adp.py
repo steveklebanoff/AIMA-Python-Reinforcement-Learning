23d41d77827567588b649803b915bd9500d3a58e
Steven A. Klebanoff <stevek@turtles.ccs.neu.edu>
2010-04-22 18:41:24 -0400

    PassiveADPAgent can now execute trials

--- passive_adp.py
+++ passive_adp.py
@@ -1,6 +1,12 @@
+import logging
+
 from aima.mdp import GridMDP, MDP
 from aima.utils import turn_left, turn_right
 from random import randint
+from time import time
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(levelname)s: %(message)s')
 
 def char_to_tuple(direction):
     switch = {
@@ -38,10 +44,7 @@ class PassiveADPAgent(object):
                        terminals=action_mdp.terminals,
                        gamma = 0.9)
         self.action_mdp = action_mdp
-        self.utility = [[None,None,None,None],
-                        [None,None,None,None],
-                        [None,None,None,None],
-                        [None,None,None,None]]
+        self.utility = { }
         self.sa_freq = { }
         self.outcome_freq = { }
 
@@ -63,23 +66,63 @@ class PassiveADPAgent(object):
     def get_move_from_policy(self, state_x, state_y):
         return self.policy[state_x][state_y]
         
-    def calculate_move(self, current_percept):
-        # policy = policy
+    def next_action(self, current_state, current_reward):
+        # policy = self.policy computed by constructor
         # MDP = mdp object.
         #          MDP.T  - transistion model (initially empty),
         #          MDP.reward - reward
         #          MDP gamma in initializer
-        # utility = u (grid) i.e. [0][0] initially empty
-        # state action frequencies = sa_freq (grid) i.e. [0][0] initially empty
+        # utility = dictionary u[(0,0)] = 0.57 etc
+        # state action frequencies = sa_freq (dict) initially empty
         # outcome frequenes given state outcome and state-action pairs = outcome_freq  initially empty
         #     dict with key being new state, value being another dict with keys being
         #     state, action pairs and values being that percentage
         # previous state, previous action = s,a
-        raise NotImplementedError
+        
+        # If we're at a terminal we don't want a next move
+        if current_state in self.mdp.terminals:
+            return False
+        
+        # Return the next action that the policy dictates
+        return self.policy[current_state]
+
+    
+    def execute_trial(self):
+        # Start at initial state
+        current_state = self.mdp.init
+        
+        # Keep going until we get to a terminal state
+        while True:
+            logging.debug('--------------------------')
+            
+            # Get reward for current state
+            current_reward = self.action_mdp.R(current_state)
+            
+            # Calculate move from current state
+            next_action = self.next_action(current_state, current_reward)
+            
+            logging.debug('Current State: %s ' % str(current_state))
+            logging.debug('Current Reward: %s ' % current_reward)
+            logging.debug('Next action: %s' % next_action)
+            
+            if next_action == False:
+                # End because next_action told us to
+                logging.debug('Next_action returned false, stopping')
+                break
+            
+            # Get new current_state
+            current_state = self.action_mdp.simulate_move(current_state, char_to_tuple(next_action))
 
 # Setup values
-# TODO: find from value iteration (?)
 policy = [['>', '>', '>', '.'],
           ['^', None, '^', '.'],
           ['^', '<', '<', '<']]
 agent = PassiveADPAgent(Fig[17,1], policy)
+
+trials = 1
+# Execute a bunch of trials
+for i in range (0,trials):
+    agent.execute_trial()
+
+logging.info('Executed %i trials:' % (trials))
+logging.info('Utilities: %s' % (agent.utility))
